d27cc39c4bb7e0be9bc3867387ea87829a3033c5
Mike Barberry <52507419+MikeBarberry@users.noreply.github.com>
2021-12-13 22:08:24 -0800

    add confirm delete dialog

--- client/src/components/content/UserContent.js
+++ client/src/components/content/UserContent.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import { Box, Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material'
+import React, { useState } from 'react';
+import { Box, Card, CardActions, CardContent, CardMedia, Button, Typography, 
+        Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions 
+} from '@mui/material'
 import { Delete } from '@mui/icons-material'
 
 
 const UserContent =  ({ handleDelete, content }) => {
+    
+    const [open,setOpen] = useState(true)
+    
+    const handleOpen = () => {
+        setOpen(true)
+    };
+
+    const handleClose = () => {
+        setOpen(false)
+    };
+
     return(
        content?.map(post => {
-            const cb = () => {handleDelete(post.id)}
+            const cb = () => { 
+                handleDelete(post.id) 
+                setOpen(false)
+            }
             return <Box sx={{display: 'flex', flex: 'auto', width: 300, height: 350, flexDirection: 'column', justifyContent: 'center', padding: '20px'}}>
                     <Card>
                         <CardMedia
@@ -25,7 +41,28 @@ const UserContent =  ({ handleDelete, content }) => {
                             </Typography>
                         </CardContent>
                         <CardActions sx={{justifyContent: 'center'}}>
-                            <Button onClick={cb}size="small"><Delete /></Button>
+                                <Button onClick={handleOpen}size="small"><Delete /></Button>
+                                <Dialog
+                                    open={open}
+                                    onClose={handleClose}
+                                    aria-labelledby="alert-dialog-title"
+                                    aria-describedby="alert-dialog-description"
+                                >
+                                    <DialogTitle id="alert-dialog-title">
+                                        {"Delete Content?"}
+                                    </DialogTitle>
+                                    <DialogContent>
+                                        <DialogContentText id="alert-dialog-description">
+                                            This action can't be undone.
+                                        </DialogContentText>
+                                    </DialogContent>
+                                    <DialogActions>
+                                        <Button onClick={handleClose}>Go Back</Button>
+                                        <Button onClick={cb} autoFocus>
+                                            OK
+                                        </Button>
+                                    </DialogActions>
+                                </Dialog>
                         </CardActions>
                     </Card>
                 </Box>

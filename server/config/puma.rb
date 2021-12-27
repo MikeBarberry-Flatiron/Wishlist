e0ff80b3b71001c7806e321c5340b21d0f1b47fa
MikeBarberry <52507419+MikeBarberry@users.noreply.github.com>
2021-12-27 05:10:00 -0800

    changes for deployment on server

--- server/config/puma.rb
+++ server/config/puma.rb
@@ -5,7 +5,7 @@
 # and maximum; this matches the default thread size of Active Record.
 #
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { 1 }
 threads min_threads_count, max_threads_count
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
@@ -19,7 +19,9 @@ port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
+
+bind "unix:///var/www/wishlists.link/server/puma.sock"
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }

68e15028038258b4c24d945c35870ca33bfc04c3
MikeBarberry <52507419+MikeBarberry@users.noreply.github.com>
2021-12-28 19:09:40 -0800

    update redux to use composewithdevtools

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
@@ -10,8 +11,5 @@ const middleware = [thunk]
 export default createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    composeWithDevTools(applyMiddleware(...middleware))
 )

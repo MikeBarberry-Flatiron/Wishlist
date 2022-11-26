59b11f53b9f9cfbe750cf017ccd5d5810d349f2c
Mike Barberry <52507419+MikeBarberry@users.noreply.github.com>
2022-11-25 22:32:33 -0800

    update login route

--- server/routes/login/login.js
+++ server/routes/login/login.js
@@ -1,60 +1,81 @@
-const { MongoClient } = require("mongodb");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-// local uses dotenv
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const client = new MongoClient(process.env.MONGO_URI);
 
-async function login(username, password) {
-  try {
-    const usercontent = client.db("wishlist").collection("usercontent");
-    const user = await usercontent.findOne({
-      username: username.toLowerCase(),
-    });
-    if (!user) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ errors: "User not found." }),
-      };
-    }
-    const isValidPassword = await bcrypt.compare(password, user.hash);
-    if (!isValidPassword) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ errors: "Wrong password." }),
-      };
-    }
-    const token = await jwt.sign(
-      { user_name: username },
-      process.env.JWT_SECRET
-    );
+exports.handler = async (event) => {
+  if (event.httpMethod === 'OPTIONS') {
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        message: "Logged in.",
-        jwt: token,
-      }),
+      headers: {
+        'access-control-allow-headers':
+          'content-type,x-amz-date,authorization,x-api-key,x-amz-security-token,origin,accept',
+        'access-control-allow-methods': 'options,post,get,put,delete',
+        'access-control-allow-origin': '*',
+      },
     };
-  } catch (error) {
-    console.error(`Error occurred during login: ${error}`);
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        errors: "An error occurred during login.",
-      }),
-    };
-  } // local needs finally with client.close(). Lambda does not
-}
-
-exports.handler = async ({ username, password }) => {
-  async function validateInputAndProceedIfOk() {
-    if (!username || username.length < 2 || !password || password.length < 8) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ errors: "Invalid username or password." }),
-      };
-    }
-    return login(username, password);
   }
-  return validateInputAndProceedIfOk();
+
+  if (event.httpMethod === 'POST') {
+    const returnValues = {
+      200 (token) {
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: 'Logged in.',
+            error: null,
+            jwt: token
+          }),
+          headers: {
+            'access-control-allow-origin': '*',
+          },
+        }
+      },
+      400 (message) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({
+          message: null,
+          error: message,
+        }),
+        headers: {
+          'access-control-allow-origin': '*',
+        },
+        }
+      },
+      500: {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: null,
+          error: 'A server error occurred during login.',
+        }),
+        headers: {
+          'access-control-allow-origin': '*',
+        },
+      }
+    }
+    try {
+      const body = JSON.parse(event.body)
+      const username = body.username.toLowerCase()
+      const { password } = body
+      const database = client.db('wishlist');
+      const collection = database.collection('usercontent');
+
+      const user = await collection.findOne({ username })
+      if (!user) {
+        return returnValues[400]('User does not exist.')
+      }
+      const isPasswordCorrect = await bcrypt.compare(password, user.hash)
+      if (!isPasswordCorrect) {
+        return returnValues[400]('Password is incorrect.')
+      }
+      const token = jwt.sign({ user }, 'w!$HL!$T3')
+      return returnValues[200](token)
+    } catch (err) {
+      console.log(`Error: ${err}`)
+      return returnValues[500]
+    }
+  }
 };
+

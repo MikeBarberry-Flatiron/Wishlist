9445591ba511f4779b958af7d1af9f076fe670cb
Mike Barberry <52507419+MikeBarberry@users.noreply.github.com>
2022-11-26 19:05:28 -0800

    Update contentActions.js

--- client/src/store/actions/contentActions.js
+++ client/src/store/actions/contentActions.js
@@ -7,7 +7,6 @@ import {
 import store from "../../store";
 
 export const getUserContent = (token) => async (dispatch) => {
-  console.log("started fetch content", token);
   const response = await fetch(
     "https://pshgvjl5aa.execute-api.us-west-2.amazonaws.com/production/api/getcontent",
     {
@@ -19,55 +18,54 @@ export const getUserContent = (token) => async (dispatch) => {
     }
   );
   const json = await response.json();
-  console.log(json.content);
   dispatch({
     type: SET_USER_CONTENT,
     payload: json.content,
   });
 };
 
-export const addContent = (request) => (dispatch) => {
-  fetch("/api/add", {
+export const addContent = (request) => async (dispatch) => {
+  const response = await fetch(
+    "https://pshgvjl5aa.execute-api.us-west-2.amazonaws.com/production/api/addcontent", 
+  {
     method: "POST",
     headers: {
       "content-type": "application/json",
     },
     body: JSON.stringify(request),
-  })
-    .then((res) => res.json())
-    .then((json) => {
-      dispatch({
-        type: ADD_CONTENT,
-        payload: json.updated,
-      });
-      dispatch({
-        type: CONTENT_SUCCESS,
-        payload: true,
-      });
-    });
+  });
+  const json = await response.json()
+  dispatch({
+    type: ADD_CONTENT,
+    payload: json.updated,
+  });
+  dispatch({
+    type: CONTENT_SUCCESS,
+    payload: true,
+  });
 };
 
-export const deleteContent = (request) => (dispatch) => {
-  fetch("/api/delete", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify(request),
-  })
-    .then((res) => res.json())
-    .then((json) => {
-      const { deletedContent, newContent } = json.updated;
-      const index = store
-        .getState()
-        .userContent.userContent.map((e) => e.id)
-        .indexOf(deletedContent);
-      dispatch({
-        type: DELETE_CONTENT,
-        payload: {
-          deleteIndex: index,
-          newContent: newContent,
-        },
-      });
+export const deleteContent = (request) => async (dispatch) => {
+  const response = await fetch(
+    "https://pshgvjl5aa.execute-api.us-west-2.amazonaws.com/production/api/deletecontent", 
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(request),
     });
+  const json = await response.json();
+  const { deletedContent, newContent } = json.updated;
+  const index = store
+    .getState()
+    .userContent.userContent.map((e) => e.id)
+    .indexOf(deletedContent);
+  dispatch({
+    type: DELETE_CONTENT,
+    payload: {
+      deleteIndex: index,
+      newContent: newContent,
+    },
+  });
 };
